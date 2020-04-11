f47141bd1c5ca4a984cab42a41cab2dfe846ecd3
Voloodya <48101401+Voloodya@users.noreply.github.com>
2020-04-11 21:25:19 +0500

    Add files via upload

---
+++ Server.py
@@ -0,0 +1,29 @@
+import socket
+import os
+import sys
+
+
+def mysend (client_socket, msg):
+	totalsend=0
+	while totalsend<len(msg):
+		sent=client_socket.send(msg[totalsend:])
+		if sent==0:
+			raise RuntimeError('broken')
+		totalsend=totalsend+sent
+
+		
+server_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('0.0.0.0',2222))
+server_socket.listen(10)
+
+def serverGet (client_socket, remote_address)
+	while True:
+		data_request=client_socket.recv(1024)
+		if not data_request or data_request.decode('utf8').strip()=='close'=='close': break
+		mysend (client_socket,data_request)
+	client_socket.close()
+
+while True:
+	client_socket, remote_address=server_socket.accept()
+	t = threading.Thread(target=serverGet, args=(client_socket, remote_address))
+    t.start()	
