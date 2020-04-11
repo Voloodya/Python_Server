321222ef6423200f9a4fa8f0980fd0dcd41f9330
Voloodya <48101401+Voloodya@users.noreply.github.com>
2020-04-11 21:43:24 +0500

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,7 +1,4 @@
 import socket
-import os
-import sys
-
 
 def mysend (client_socket, msg):
 	totalsend=0
@@ -11,19 +8,24 @@ def mysend (client_socket, msg):
 			raise RuntimeError('broken')
 		totalsend=totalsend+sent
 
-		
-server_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('0.0.0.0',2222))
-server_socket.listen(10)
 
-def serverGet (client_socket, remote_address):
+def serverGet (client_socket, remote_address)
 	while True:
 		data_request=client_socket.recv(1024)
 		if not data_request or data_request.decode('utf8').strip()=='close'=='close': break
 		mysend (client_socket,data_request)
 	client_socket.close()
 
-while True:
-	client_socket, remote_address=server_socket.accept()
-	t = threading.Thread(target=serverGet, args=(client_socket, remote_address))
-    t.start()	
+server_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('0.0.0.0',2222))
+server_socket.listen(10)
+
+def main():
+
+	while True:
+		client_socket, remote_address=server_socket.accept()
+		t = threading.Thread(target=serverGet, args=(client_socket, remote_address))
+		t.start()
+		
+if __name__ == "__main__":
+	main()
